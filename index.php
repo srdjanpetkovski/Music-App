8034f394fd594fd8f71821831d678170fad10e76
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-10-24 16:36:38 +0200

    header i footer html

---
+++ index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>MusicApp</title>
+        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
+        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
+        <link rel="stylesheet" href="css/style.css">
+    </head>
+    <body>
+        <!-- nav -->
+        <nav>
+            <a href="index.php" class="logo"><span>Music</span>App</a>
+            <ul>
+                <li><a href="index.php"></a>Home</li>
+                <li><a href="how-it-works.php"></a>How it works</li>
+                <li><a href="subscription.php"></a>Subsrciption</li>
+            </ul>
+        </nav>
+
+        <!-- footer -->
+        <footer>
+        <a href="index.php" class="logo"><span>Music</span>App</a>
+        <p>&copy; 2021. All right reserved</p>
+        <div class="icons">
+            <a href=""><i class="fab fa-facebook-square"></i></a>
+            <a href=""><i class="fab fa-instagram"></i></a>
+            <a href=""><i class="fab fa-soundcloud"></i></a>
+            <a href=""><i class="fab fa-youtube"></i></a>
+        </div>
+        </footer>
+    </body>
+</html> 
