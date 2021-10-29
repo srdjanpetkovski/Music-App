1dc23dfbe9f16e600e86626365948afecb2798a9
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-10-29 19:22:39 +0200

    header i footer include

--- index.php
+++ index.php
@@ -1,34 +1,5 @@
-<!DOCTYPE html>
-<html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <title>MusicApp</title>
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.2/css/all.css" integrity="sha384-oS3vJWv+0UjzBfQzYUhtDYW+Pj2yciDJxpsK1OYPAYjqT085Qq/1cq5FLXAZQ7Ay" crossorigin="anonymous">
-        <link href="https://fonts.googleapis.com/css2?family=Montserrat:wght@300;400;600;700&display=swap" rel="stylesheet">
-        <link rel="stylesheet" href="css/style.css">
-    </head>
-    <body>
-        <!-- nav -->
-        <nav>
-            <a href="index.php" class="logo"><span>Music</span>App</a>
-            <ul>
-                <li><a href="index.php"></a>Home</li>
-                <li><a href="how-it-works.php"></a>How it works</li>
-                <li><a href="subscription.php"></a>Subsrciption</li>
-            </ul>
-        </nav>
+<?php include "partials/header.php"; ?>
 
-        <!-- footer -->
-        <footer>
-        <a href="index.php" class="logo"><span>Music</span>App</a>
-        <p>&copy; 2021. All right reserved</p>
-        <div class="icons">
-            <a href=""><i class="fab fa-facebook-square"></i></a>
-            <a href=""><i class="fab fa-instagram"></i></a>
-            <a href=""><i class="fab fa-soundcloud"></i></a>
-            <a href=""><i class="fab fa-youtube"></i></a>
-        </div>
-        </footer>
-    </body>
-</html> 
+        <h1 style="font-size:9rem; text-align:center;">Home</h1>
+
+<?php include "partials/footer.php"; ?>
---
+++ subscription.php
@@ -0,0 +1,5 @@
+<?php include "partials/header.php"; ?>
+
+        <h1 style="font-size:9rem; text-align:center;">Subscription</h1>
+
+<?php include "partials/footer.php"; ?>
