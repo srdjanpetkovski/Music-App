eb09ea6b6033bd9b7d860521489b6fe0b80fb652
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-10-31 18:56:04 +0100

    howItWorks

--- how-it-works.php
+++ how-it-works.php
@@ -1,5 +1,25 @@
 <?php include "partials/header.php"; ?>
 
-        <h1 style="font-size:9rem; text-align:center;">How it works</h1>
+<!-- howItWorks -->
+        <section class="howItWorks">
+                <h2 class="container">How it works</h2>
+                <article class="container">
+                        <div>
+                                <i class="fas fa-user-circle"></i>
+                                <h3>Create an account</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quia cupiditate atque, sed et dolor mollitia blanditiis explicabo illum architecto!</p>
+                        </div>
+                        <div>
+                        <i class="fas fa-drum"></i>
+                                <h3>Choose plan</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quia cupiditate atque, sed et dolor mollitia blanditiis explicabo illum architecto!</p>
+                        </div>
+                        <div>
+                        <i class="fas fa-cloud-download-alt"></i>
+                                <h3>Download music</h3>
+                                <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Voluptate quia cupiditate atque, sed et dolor mollitia blanditiis explicabo illum architecto!</p>
+                        </div>
+                </article>
+        </section>
 
 <?php include "partials/footer.php"; ?>
