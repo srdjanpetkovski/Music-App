b1ab86396cda9af7b9b350d628de7f702bb639ae
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-10-31 18:22:30 +0100

    unlimited html

--- index.php
+++ index.php
@@ -12,4 +12,15 @@
                 </div>
         </header>
 
+<!-- unlimited -->
+        <section class="unlimited container">
+                <article>
+                        <h2>Unlimited Access to 100K tracks</h2>
+                </article>
+                <article>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quidem officia mollitia eveniet voluptate animi earum cum itaque excepturi saepe. Illum quas magnam aperiam nihil dolore sit id odit, officia, temporibus ut blanditiis ratione delectus maiores. Cupiditate, praesentium. Laborum sapiente, facere culpa debitis tenetur laudantium maiores repellat praesentium cumque. Dicta?</p>
+                        <button>Try it now</button>
+                </article>
+        </section>
+
 <?php include "partials/footer.php"; ?>
