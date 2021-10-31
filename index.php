0ba79fb0bf70909259774d5f0e73d29a6f156024
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-10-31 18:13:26 +0100

    header css

--- index.php
+++ index.php
@@ -1,5 +1,15 @@
 <?php include "partials/header.php"; ?>
 
-        <h1 style="font-size:9rem; text-align:center;">Home</h1>
+<!-- header -->
+        <header class="mainHeader">
+                <div>
+                        <h1><span>Music</span> for everyone</h1>
+                        <p>Lorem ipsum dolor sit amet consectetur, adipisicing elit. Reprehenderit sed dolor ad pariatur neque quam, consequuntur dolorum maxime autem exercitationem.</p>
+                        <button>Start for trial</button>
+                </div>
+                <div>
+                        <img src="img/header.png" alt="">
+                </div>
+        </header>
 
 <?php include "partials/footer.php"; ?>
