7caab60f7d7f9dfe5d64082506c193efd4912e0a
srdjanpetkovski <srdjanpetkovski@gmail.com>
2021-11-01 20:25:13 +0100

    subscription html i css

--- subscription.php
+++ subscription.php
@@ -1,5 +1,29 @@
 <?php include "partials/header.php"; ?>
 
-        <h1 style="font-size:9rem; text-align:center;">Subscription</h1>
+<!-- subscription -->
+        <section class="container subscription">
+                <article>
+                        <h2>Our concept & artists</h2>
+                        <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Laudantium quasi reiciendis aspernatur nihil, ipsum et quas doloremque impedit, ipsam placeat hic earum illum enim deserunt. Veniam aliquam sequi necessitatibus natus dolores, expedita eos, adipisci doloribus, sed ipsum quod soluta nam?</p>
+                </article>
+                <article>
+                        <div>
+                                <img src="img/subscription1.jpg" alt="">
+                                <h3>Soul music</h3>
+                        </div>
+                        <div>
+                                <img src="img/subscription2.jpg" alt="">
+                                <h3>Live concerts</h3>
+                        </div>
+                        <div>
+                                <img src="img/subscription3.jpg" alt="">
+                                <h3>DJ sets</h3>
+                        </div>
+                        <div>
+                                <img src="img/subscription4.jpg" alt="">
+                                <h3>Live streams</h3>
+                        </div>
+                </article>
+        </section>
 
 <?php include "partials/footer.php"; ?>
